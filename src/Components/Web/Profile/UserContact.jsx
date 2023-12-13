3f225b9af9ea2200d8eb17732b195b136855c25e
Ibrahim Maali <maaliibrahim2022@gmail.com>
2023-12-13 14:42:52 +0200

    first commit

---
+++ src/Components/Web/Profile/UserContact.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import { UserContext } from './../Context/UserContext';
+
+export default function UserContact() {
+    let {userDetails,setUserDetails,loading}=useContext(UserContext);
+    console.log(userDetails);
+    if(loading) {
+        return <h2>Loading .........</h2>
+       }
+  return (
+    <>
+    <h2>{userDetails.email}</h2>
+    <h2>{userDetails.phone?userDetails.phone:'phone number was not provided'}</h2>
+    </>
+  )
+}
